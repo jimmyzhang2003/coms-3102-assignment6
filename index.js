99ab56c5cf99cc3acb1f4e464520efd8e1695832
Jimmy Zhang <jimmyzhang2003@gmail.com>
2024-04-07 16:19:23 -0400

    Added logic for saving user input as txt file

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
-
 import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
@@ -23,8 +17,13 @@ inquirer
 
 		let image = qr.image(url, { type: "png" });
 
-		// use fs to save user input
+		// use fs to save QR code as png file
 		image.pipe(fs.createWriteStream("output/qr_img.png"));
+
+		// use fs to save user input as txt file
+		fs.writeFile("output/URL.txt", url, (err) => {
+			if (err) throw err;
+		});
 	})
 	.catch((error) => {
 		console.error(error);
